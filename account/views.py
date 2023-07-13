e5fd28eaf088a8b5bf1cdf8528c6b67bddf092bb
Sardorbekmaxmud <sardorbekmaxmudov33@gmail.com>
2023-07-13 13:02:42 +0500

    small change

--- account/views.py
+++ account/views.py
@@ -3,8 +3,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
-# from .serializer import *
-# from .models import *
+from .serializer import *
+from .models import *
 
 # Create your views here.
 # class All
